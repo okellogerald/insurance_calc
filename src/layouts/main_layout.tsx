eabc5ad9bcd58793f6f84f1470e839b8bc5c742a
Okello Gerald <okellogeralddev@gmail.com>
2023-12-08 17:10:25 +0300

    minor updates

--- src/layouts/main_layout.tsx
+++ src/layouts/main_layout.tsx
@@ -7,10 +7,8 @@ interface MainLayoutProps {
 export default class MainLayout extends Component<MainLayoutProps> {
     render() {
         return (
-            <main className="flex flex-col items-center justify-center h-screen p-5 prose">
-                <div className="flex flex-col items-center gap-y-2" >
+            <main className="flex flex-col items-center justify-center h-screen p-5 max-w-md prose">
                     {this.props.children}
-                </div>
             </main>
         )
     }
